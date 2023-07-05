dc95bf7c57ed52e719b5818d3aa7e7ffdd6e6eaf
JoeTheorium <fernandoandresraggio@gmail.com>
2023-07-04 22:09:53 -0300

    Create hero section

---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
