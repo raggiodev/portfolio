8d49ef36576bb76f93fc0bc495de8622e53e42da
JoeTheorium <fernandoandresraggio@gmail.com>
2023-07-04 22:22:51 -0300

    Create Hero content

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero: React.FC = () => {
+  const one = <h1>Hi, my name is</h1>;
+  const two = <h2 className="big-heading">Fernando Andres Raggio</h2>;
+  const three = <h3 className="big-heading">PALABRAS QUE CAMBIAN.</h3>;
+  const four = (
+    <>
+      <p>
+        I’m a software lorem ipsum y tal y cual.
+      </p>
+    </>
+  );
+  const five = (
+    <a
+      className="link"
+      href="https://www.github.com/JoeTheorium"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Know me!
+    </a>
+  );
 
-export default Hero
+  const items = [one, two, three, four, five];
+
+  return (
+    <section>
+      
+    </section>
+  );
+};
+
+export default Hero;
