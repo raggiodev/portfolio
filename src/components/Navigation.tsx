77c618b8aad138b93f354c13c3bfb4fe3b376e11
JoeTheorium <fernandoandresraggio@gmail.com>
2023-07-24 23:08:14 -0300

    add Navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,34 @@
+// components/Navigation.tsx
+import React from 'react';
+
+const Navigation: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#blog">Blog</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="#resume">Resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
