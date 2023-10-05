b6a88fd611c1e6b9118a92093b3d036947d61a2d
JoeTheorium <fernandoandresraggio@gmail.com>
2023-10-05 00:16:34 -0300

    Update CSS & add animations in About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,56 @@
+import {motion} from "framer-motion";
 import Cursor from "./Cursor";
 
 const About = () => {
   return (
-    <section id='about'>
-      <h4>Hi, my name is</h4>
-      <h1>Fernando Andres Raggio</h1>
-      <h2>Full Stack Developer</h2>
+    <motion.article id='about' whileInView={"visible"}>
+      <motion.h4
+      initial={{
+        opacity: 0,
+        x: -50,
+      }}
+      variants={{
+        visible: {
+        opacity: 1,
+        x: 0,
+        transition:{
+          duration: 1,
+          delay: 0.8,
+        }}
+      }}
+      >Hi, my name is</motion.h4>
+      <motion.h1
+      initial={{
+        opacity: 0,
+        x: -50,
+      }}
+      variants={{
+        visible: {
+        opacity: 1,
+        x: 0,
+        transition:{
+          duration: 1,
+          delay: 1.2,
+        }}
+      }}
+      >Fernando Andres Raggio</motion.h1>
+      <motion.h2
+      initial={{
+        opacity: 0,
+        x: -50,
+      }}
+      variants={{
+        visible: {
+        opacity: 1,
+        x: 0,
+        transition:{
+          duration: 1,
+          delay: 1.6,
+        }}
+      }}
+      >Full Stack Developer</motion.h2>
       <Cursor/>
-    </section>
+    </motion.article>
   )
 }
 
