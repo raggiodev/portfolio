cdccbfa02fae7fed16148b081b645e05c129f473
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2025-04-09 00:19:21 -0300

    Modif. Contact de Midudev por el mío propio - v. 1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,9 @@ const Contact = () => {
   return (
       <contact>
         <div>
-          <p>Me llamo Miguel Ángel pero mis amigos me llaman midu. Empecé en la programación con un Amstrad, tenía 10 años. Actualmente estoy liderando equipos de desarrollo en multinacionales.</p>
-          <p>Algunos de mis éxitos incluyen colaborar con Mozilla para el desarrollo de las primeras apps en su sistema FirefoxOS. Aunque hoy está desaparecido fue un gran avance en el mundo del desarrollo web.</p>
-          <p>Como creador de contenido, cuento con el canal de habla hispana más visto del mundo en la categoría de Software & Game Development en Twitch. Mi objetivo es mejorar la empleabilidad de la comunidad hispana y el acceso a contenido de calidad.</p>
+          <p>Me llamo Fernando Andres Raggio pero mis amigos me llaman Fer. Empecé en la programación con Arduino, tenía 14 años. Actualmente estoy trabajando con equipos de desarrollo.</p>
+          <p>Algunos de mis éxitos incluyen colaborar con empresas para dar clases; mis proyectos exitosos para clientes; los que tuvieron muchas estrellas en GitHub; que Midudev me acepte un fork; etc.</p>
+          <p>Como desarrollador, mi objetivo es estar y ayudar a lograr avances en una gran empresa para impulsar mejoras a gran escala.</p>
           <img src="" alt="Una foto mía a la derecha inclinada un poco a la derecha con un marquito lindo." />
         </div>
       </contact>
